86e66f15d3db6f6576e182ef1fa193e3aa5d9276
Thiago Dias <thiagofellipe055@gmail.com>
2025-07-04 15:40:58 -0300

    Projeto final

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,64 @@
-alert("Hello, Detona Ralph!");
+const state = {
+    view: {
+        square: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVeolocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        curretTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+
+    if (state.values.curretTime <= 0) {
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+        alert("Game Over! Seu score final é: " + state.values.result);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.square.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.square[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.view.square.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+}
+
+function init() {
+    addListenerHitBox();
+}
+
+init();
